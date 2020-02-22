74782ba84bd39df2e24d60e56e9e20aab2ceb804
Antoine.D <43401777+antoineDurand82@users.noreply.github.com>
2020-02-22 16:40:48 +0100

    Update init_serveur.sh

--- init_serveur.sh
+++ init_serveur.sh
@@ -11,8 +11,8 @@ mkdir /saveBackup
 echo "192.168.56.111 client-1">>/etc/hosts
 echo "192.168.56.112 client-2">>/etc/hosts
 
-echo "/backup client-1(rw,no_root_squash) \ *.local.domain.edu(rw,no_root_squash)">>/etc/exports
-echo "/backup client-2(rw,no_root_squash) \ *.local.domain.edu(rw,no_root_squash)">>/etc/exports
+echo "/srv/backup client-1(rw,no_root_squash) \ *.local.domain.edu(rw,no_root_squash)">>/etc/exports
+echo "/srv/backup client-2(rw,no_root_squash) \ *.local.domain.edu(rw,no_root_squash)">>/etc/exports
 
 
 systemctl enable rpcbind
@@ -23,7 +23,7 @@ systemctl start nfs-server
 touch auto.sh
 chmod +x auto.sh
 
-echo "sudo cp -r /backup /srv/saveBackup">>/srv/backup_centos8/auto.sh
+echo "sudo cp -r /srv/backup /srv/saveBackup">>/srv/backup_centos8/auto.sh
 
 mv /srv/saveBackup/auto.sh /etc/cron.daily/
 
