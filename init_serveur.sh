ff5a8efa44ebaba453275e5ced103f92dd8b66a7
Antoine.D <43401777+antoineDurand82@users.noreply.github.com>
2020-02-22 16:13:57 +0100

    Update init_serveur.sh

--- init_serveur.sh
+++ init_serveur.sh
@@ -4,14 +4,15 @@ dnf install nfs-utils -y
 
 dracut -f -v
 
+cd /srv/
 mkdir /backup
+mkdir /saveBackup
 
-echo -e "\n\nVeuillez fournir l'adresse ip de votre client:"
-read ip_client
+echo "192.168.56.111 client-1">>/etc/hosts
+echo "192.168.56.112 client-2">>/etc/hosts
 
-echo "$ip_client client">>/etc/hosts
-
-echo "/backup client(rw,no_root_squash) \ *.local.domain.edu(rw,no_root_squash)">>/etc/exports
+echo "/backup client-1(rw,no_root_squash) \ *.local.domain.edu(rw,no_root_squash)">>/etc/exports
+echo "/backup client-2(rw,no_root_squash) \ *.local.domain.edu(rw,no_root_squash)">>/etc/exports
 
 
 systemctl enable rpcbind
@@ -22,14 +23,8 @@ systemctl start nfs-server
 touch auto.sh
 chmod +x auto.sh
 
-echo -e "\n\nVeuillez fournir le chemin complet jusqu'à votre dossier où vous voulez avoir une backup de votre dossier partager \nPar exemple '/home/antoine/Desktop'. Pensez à retirer le dernier '/' se trouvant après le dernier dossier"
-read cheminbackup
+echo "sudo cp -r /backup /srv/saveBackup">>/srv/backup_centos8/auto.sh
 
-echo -e "\n\nVeuillez fournir le chemin complet jusqu'à votre dossier contenant le fichier 'init_serveur.sh' que vous venez de lancer"
-read cheminfichier
-
-echo "sudo cp -r /backup $cheminbackup">>$cheminfichier/auto.sh
-
-mv $cheminfichier/auto.sh /etc/cron.daily/
+mv /srv/saveBackup/auto.sh /etc/cron.daily/
 
 echo -e "\n\nL'installion est terminée, bravo"
