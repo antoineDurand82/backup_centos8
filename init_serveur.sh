ea109f0987ff7a8bfdeef082abd892c1d321a27b
Antoine.D <43401777+antoineDurand82@users.noreply.github.com>
2020-02-23 05:00:40 +0100

    Update init_serveur.sh

--- init_serveur.sh
+++ init_serveur.sh
@@ -8,11 +8,17 @@ cd /srv/
 mkdir backup
 mkdir saveBackup
 
+mount --bind /srv/backup /srv/backup
+
+echo "/srv/backup /srv/backups  none   bind   0   0">>/etc/fstab
+
 echo "192.168.56.111 client-1">>/etc/hosts
 echo "192.168.56.112 client-2">>/etc/hosts
 
 echo "/srv/backup client-1(rw,no_root_squash) \ *.local.domain.edu(rw,no_root_squash)">>/etc/exports
 echo "/srv/backup client-2(rw,no_root_squash) \ *.local.domain.edu(rw,no_root_squash)">>/etc/exports
+
+exportfs -ra
 
 
 systemctl enable rpcbind
